7810e0283d6017c8968a6c00fbfadf707ea1e89d
proudloyaltier <timediamail@gmail.com>
2019-10-16 09:37:22 -0400

    Updates

---
+++ precache-manifest.33e66815b099c9a1cc3bde07ba8bbdc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08451e3adf37987a8f82c50e5efba3c3",
+    "url": "/monitor/index.html"
+  },
+  {
+    "revision": "4b7c7b16e00490f100ea",
+    "url": "/monitor/static/css/main.6abd79f8.chunk.css"
+  },
+  {
+    "revision": "bfe34562d7e75ee6d582",
+    "url": "/monitor/static/js/2.7b8fe112.chunk.js"
+  },
+  {
+    "revision": "4b7c7b16e00490f100ea",
+    "url": "/monitor/static/js/main.7e617f4e.chunk.js"
+  },
+  {
+    "revision": "c47bcf7e51e75ddd7cde",
+    "url": "/monitor/static/js/runtime-main.6a4c1504.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97b43d01702a2be34a8ae3be933055a4.js"
+  "/monitor/precache-manifest.33e66815b099c9a1cc3bde07ba8bbdc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monitor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
